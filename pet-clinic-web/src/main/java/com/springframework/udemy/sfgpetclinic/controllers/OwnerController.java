23183097e45e51d4e09f3b8a1306bd1097a9aa79
Anuj Prajapati <anuj.prajapati@yantriks.com>
2020-01-16 19:33:39 +0530

    Showing list of vets and owners closes #15 closes #16

--- pet-clinic-web/src/main/java/com/springframework/udemy/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/udemy/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package com.springframework.udemy.sfgpetclinic.controllers;
 
+import com.springframework.udemy.sfgpetclinic.services.map.OwnerMapServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerMapServiceMap ownerMapServiceMap;
+
+    public OwnerController(OwnerMapServiceMap ownerMapServiceMap) {
+        this.ownerMapServiceMap = ownerMapServiceMap;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerMapServiceMap.findAll());
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/springframework/udemy/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/udemy/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.springframework.udemy.sfgpetclinic.controllers;
 
+import com.springframework.udemy.sfgpetclinic.services.map.VetMapServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetMapServiceMap vetMapServiceMap;
+
+    public VetController(VetMapServiceMap vetMapServiceMap) {
+        this.vetMapServiceMap = vetMapServiceMap;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetMapServiceMap.findAll());
         return "vets/index";
     }
 }
