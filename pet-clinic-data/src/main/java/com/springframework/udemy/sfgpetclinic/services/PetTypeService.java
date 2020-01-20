08746cb0577f71a7ef6946535b7d4cda416a5e20
Anuj Prajapati <anuj.prajapati@yantriks.com>
2020-01-20 16:09:59 +0530

    Added PetType Services closes #27

---
+++ pet-clinic-data/src/main/java/com/springframework/udemy/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.udemy.sfgpetclinic.services;
+
+import com.springframework.udemy.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
